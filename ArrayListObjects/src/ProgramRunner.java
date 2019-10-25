73f2783f286b29420941e59124a9efd05e28e24c
michael.nguyen <michael.nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2019-10-25 11:43:56 -0600

    Array

--- ArrayListObjects/src/ProgramRunner.java
+++ ArrayListObjects/src/ProgramRunner.java
@@ -5,12 +5,25 @@ public class ProgramRunner
 
 		public static void main(String[] args)
 			{
-				ArrayList<Programs>TVShows = new ArrayList<Programs>();	
-				TVShows.add (new Programs("Spongebob Squarepants", "Comedy", 12));
-				TVShows.add (new Programs("Stranger Things", "Sci-Fi", 3 ));
-				TVShows.add (new Programs("The Simpsons", "Comedy", 30));
-				TVShows.add (new Programs("Flash", "Sci-fi", 5));
-				TVShows.add (new Programs("The Office", "Comedy", 9));
+				printList();
 			}
+		public static void printList()
+		{
+			ArrayList<Programs>TVShows = new ArrayList<Programs>();	
+			TVShows.add (new Programs("Spongebob Squarepants", "Comedy", 12 ));
+			TVShows.add (new Programs("Stranger Things", "Sci-Fi", 3  ));
+			TVShows.add (new Programs("The Simpsons", "Comedy", 30 ));
+			TVShows.add (new Programs("The Flash", "Sci-fi", 6 ));
+			TVShows.add (new Programs("The Office", "Comedy", 9 ));
+			for( Programs n : TVShows)
+				{	
+					System.out.print(n.getTitle() + ", ");
+					n.setNumberOfSeasons(n.getNumberOfSeasons() + 1);
+					System.out.println(n.getNumberOfSeasons());
+					
+				}
+		
+			
+		}
 
 	}
