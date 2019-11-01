9abb10e7689ec871ce0b0331de12a6688b0f4214
dung <dung@dungs-mbp.domain>
2019-11-01 12:22:12 -0600

    Objects

--- ArrayListObjects/src/ProgramRunner.java
+++ ArrayListObjects/src/ProgramRunner.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ProgramRunner
 	{
@@ -15,13 +18,36 @@ public class ProgramRunner
 			TVShows.add (new Programs("The Simpsons", "Comedy", 30 ));
 			TVShows.add (new Programs("The Flash", "Sci-fi", 6 ));
 			TVShows.add (new Programs("The Office", "Comedy", 9 ));
+			
+			
+			int largestWord = Integer.MIN_VALUE; 
+			
+			for(int i = 0; i < TVShows.size(); i++) {
+				if(largestWord < indexOf(TVShows.get(i).getTitle()))
+			}
+			
+			
+			
 			for( Programs n : TVShows)
 				{	
 					System.out.print(n.getTitle() + ", ");
 					n.setNumberOfSeasons(n.getNumberOfSeasons() + 1);
 					System.out.println(n.getNumberOfSeasons());
-					
+				
 				}
+		
+		
+//			int largestString = TVShows.size();
+//			int index = 0;
+//			for (int i = 0; i < TVShows.size(); i++ )
+//			{
+//				if(TVShows.size() >  largestString)
+//				{
+//					largestString = TVShows.size();
+//					index = i;
+//				}
+//			}
+//			System.out.println();
 		
 			
 		}
